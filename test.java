f5ffc2d22ca9893fb23eeee7fd0954aa6944f7c1
lxq <1399167312@qq.com>
2024-05-03 17:26:00 +0800

    f

---
+++ test.java
@@ -0,0 +1,13 @@
+package 蓝桥杯;
+import java.util.Arrays;
+public class test {
+    public static void main(String[] args) {
+//      int a[][] = {{1,2},{3,4},{5,6}};
+//        System.out.println(Arrays.deepToString(a));
+//        String c = "jfxfxcghjh";
+//        char v[] = c.toCharArray();
+        int a = 123;
+        int b = 21;
+        System.out.printf("%4d %04d",a,b);
+    }
+}
